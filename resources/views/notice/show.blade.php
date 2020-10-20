07db18a0b877667fb2286d1e71eaf0328e638d31
Onthesys <ottogi99@gmail.com>
2020-10-20 16:03:04 +0900

    2020-10-20 3rd commit

--- resources/views/notice/show.blade.php
+++ resources/views/notice/show.blade.php
@@ -29,7 +29,7 @@
       </div>
       <div class="input-group input-group-lg" style="padding-bottom:10px;">
         <span class="input-group-addon" style="width:20%; font-size:13px;">내용</span>
-        <span class="form-control" style="height:200px;">{!! nl2br($notice->content) !!}</span>
+        <span class="form-control" style="height:auto; min-height:200px;">{!! nl2br($notice->content) !!}</span>
       </div>
       <div class="input-group input-group-lg" style="padding-bottom:10px;">
         <span class="input-group-addon" style="width:20%; font-size:13px;">첨부파일</span>
