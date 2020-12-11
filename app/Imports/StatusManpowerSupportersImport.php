5dcff5667900297ad440fde62761d7b2b860cfbc
Onthesys <ottogi99@gmail.com>
2020-12-11 14:51:55 +0900

    2020-12-11 인력지원반 지원현황-작업시작일이 종료일보다 클때 처리

--- app/Imports/StatusManpowerSupportersImport.php
+++ app/Imports/StatusManpowerSupportersImport.php
@@ -138,8 +138,6 @@ class StatusManpowerSupportersImport implements ToModel, WithStartRow, WithValid
 
                     if ($this->is_valid_numeric($value)){
                         $business_year = Carbon::createFromDate($value)->year;
-                        Log::debug($business_year);
-                        Log::debug(now()->format('Y'));
                         if (!($business_year == now()->format('Y'))){
                             $onFailure('당해년도 데이터만 입력할 수 있습니다.: '.$value);
                             return;
