7cb63fe4de5f74f6454d9600fcff0a29b182d71c
Onthesys <ottogi99@gmail.com>
2020-12-11 14:15:22 +0900

    2020-12-11 test++++

--- app/Imports/StatusManpowerSupportersImport.php
+++ app/Imports/StatusManpowerSupportersImport.php
@@ -315,8 +315,11 @@ class StatusManpowerSupportersImport implements ToModel, WithStartRow, WithValid
 
                     // 2020-12-11 작업시작일은 작업종료일보다 클수 없다는 조건 추가 : 작업시작일이 큰 경우 아래 중복 체크에서 누락됨.
 
-                    $start_date = Carbon::createFromDate('Y-m-d', $job_start_date);
-                    $end_date = Carbon::createFromDate('Y-m-d', $job_end_date);
+                    Log::debug('작업시작일:'.$job_start_date);
+                    Log::debug('작업종료일:'.$job_end_date);
+
+                    $start_date = Carbon::createFromFormat('Y-m-d', $job_start_date);
+                    $end_date = Carbon::createFromFormat('Y-m-d', $job_end_date);
 
                     if ($start_date->greaterThan($end_date)) {
                         Log::debug('작업시작일:'.$start_date->toDateTimeString);
